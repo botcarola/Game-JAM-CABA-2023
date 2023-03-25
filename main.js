29bce77379ba2c592218b3fc496f39a9dccec830
Gabriel Rojas <gabriel.johann.rojas369@gmail.com>
2023-03-25 14:37:33 -0300

    audio arreglado, botón para agrandar la pantalla agregado :)

--- main.js
+++ main.js
@@ -30,45 +30,58 @@ const mute = document.querySelector('.mute');
 const save = document.querySelector('.save');
 
 
-// if (localStorage.getItem('mute') === 'true') {
-//     mute.checked = true;
-//     audio.volume = 0;
-// } else {
-//     mute.checked = false;
-//     audio.volume = localStorage.getItem('volumen') || 0.5;
-//     volumen.value = localStorage.getItem('volumen') || 0.5;
-// }
+if (localStorage.getItem('mute') === 'true') {
+    mute.checked = true;
+    audio.volume = 0;
+} else {
+    mute.checked = false;
+    audio.volume = localStorage.getItem('volumen') || 0.5;
+    volumen.value = localStorage.getItem('volumen') || 0.5;
+}
 
-// save.addEventListener('click', () => {
+save.addEventListener('click', () => {
 
-//     localStorage.setItem('mute', mute.checked);
-//     localStorage.setItem('volumen', volumen.value);
-// });
+    localStorage.setItem('mute', mute.checked);
+    localStorage.setItem('volumen', volumen.value);
+});
 
 
-// window.addEventListener('beforeunload', () => {
-//     if (!save.clicked) {
-//       audio.pause();
-//       localStorage.setItem('audioCurrentTime', audio.currentTime);
-//     }
-//   });
+window.addEventListener('beforeunload', () => {
+    if (!save.clicked) {
+      audio.pause();
+      localStorage.setItem('audioCurrentTime', audio.currentTime);
+    }
+  });
   
-//   if (localStorage.getItem('audioCurrentTime')) {
-//     audio.currentTime = localStorage.getItem('audioCurrentTime');
-//   }
+  if (localStorage.getItem('audioCurrentTime')) {
+    audio.currentTime = localStorage.getItem('audioCurrentTime');
+  }
   
-// volumen.addEventListener('input', () => {
-//     if (!mute.checked) {
-//       audio.volume = volumen.value;
-//     }
-// });
+volumen.addEventListener('input', () => {
+    if (!mute.checked) {
+      audio.volume = volumen.value;
+    }
+});
     
-// mute.addEventListener('change', () => {
-//     if (mute.checked) {
-//       audio.volume = 0;
-//     } else {
-//       audio.volume = volumen.value;
-//     }
-// });
+mute.addEventListener('change', () => {
+    if (mute.checked) {
+      audio.volume = 0;
+    } else {
+      audio.volume = volumen.value;
+    }
+});
 
 audio.play();
+
+
+
+// Pantalla completa
+const fullscreen = document.querySelector('.window');
+
+fullscreen.addEventListener('click', () => {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    document.documentElement.requestFullscreen();
+  }
+});
